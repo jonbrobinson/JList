115d6a05695d09a11276052ddda8eba66d085a6f
jonathan robinson <jonbrobinson@gmail.com>
2014-09-13 23:25:00 -0500

    Added general layout

--- public/ad_home.php
+++ public/ad_home.php
@@ -12,9 +12,9 @@
     <title>Theme Template for Bootstrap</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
     <!-- Bootstrap theme -->
-    <link href="../../dist/css/bootstrap-theme.min.css" rel="stylesheet">
+    <link href="/css/bootstrap-theme.min.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
     <link href="theme.css" rel="stylesheet">
