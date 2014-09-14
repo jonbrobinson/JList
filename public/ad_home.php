e44fd309960f717a89af6ddc36d71c7c08fcfa6b
jonathan robinson <jonbrobinson@gmail.com>
2014-09-14 12:17:39 -0500

    Deleted Spacing and Code. Inserted Title

--- public/ad_home.php
+++ public/ad_home.php
@@ -1,16 +1,30 @@
 <? include_once 'header.php';?>
 
-    <div class="container theme-showcase" role="main">
+  <div class="container theme-showcase" role="main">
 
-      <!-- Main jumbotron for a primary marketing message or call to action -->
-      <div class="jumbotron">
-        <h1>J List</h1>
-        <h3>***Disclaimer - Demo Site***</h3>
-        <p>This is not a real website. This site was built to demonstra my skills using PHP HTML CSS and Twitter bootstrap.  PLese enjoy the site as an art piece.</p>
-      </div>
+    <!-- Main jumbotron for a primary marketing message or call to action -->
+    <div class="jumbotron">
+      <h1>J List</h1>
+      <h3>***Disclaimer - Demo Site***</h3>
+      <p>This is not a real website. This site was built to demonstra my skills using PHP HTML CSS and Twitter bootstrap.  PLese enjoy the site as an art piece.</p>
+    </div>
+
+    <table class="table">
+      <tr>
+        <th>Ad Title</th>
+        <th>Created By</th>
+        <th>Created Date</th>
+        <th>Contat</th>
+      </tr>
+      <tr>
+        <td>First Ad</td>
+        <td>Jon Rob</td>
+        <td>Todays Date</td>
+        <td>myEmail@mail.com</td>
+      </tr>
+    </table>
 
 
-
-    </div> <!-- /container -->
+  </div> <!-- /container -->
 
 <? include_once 'footer.php';?>
