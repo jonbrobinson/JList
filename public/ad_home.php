250e39a00a910b47af3abb2bfaa6af49a36b704a
jonathan robinson <jonbrobinson@gmail.com>
2014-09-15 00:02:21 -0500

    Added Feature Addsection

--- public/ad_home.php
+++ public/ad_home.php
@@ -25,25 +25,33 @@
       <p>This is not a real website. This site was built to demonstra my skills using PHP HTML CSS and Twitter bootstrap.  Plese enjoy the site as an art piece. And if you are looking for a PHP programmer please feel to reach out to me.</p>
       <p>Also check out my porfolio page <a href="http://jonbrobinson.com">Jonbroinson.com</a></p>
     </div>
+    <div class="col-md-3">
+      <h3><i class="fa fa-star"></i> Featured Ad</h3>
+      <h5><?= $ads[2][1];?></h5>
+      <h5><?= $ads[2][2];?></h5>
+      <a href="ad_view.php?id=2">Visit</a>
 
-    <table class="table">
-      <tr>
-        <th>Category</th>
-        <th>Ad Title</th>
-        <th>Created By</th>
-        <th>Created Date</th>
-        <th>Contat</th>
-      </tr>
-      <?php foreach($ads as $index => $ad):?>
-      <tr>
-        <td><?= $ad[0];?></td>
-        <td><?= $ad[1];?></td>
-        <td><?= $ad[3];?></td>
-        <td><?= $ad[5];?></td>
-        <td><?= $ad[4];?></td>
-      </tr>
-      <?php endforeach;?>
-    </table>
+    </div>
+    <div class="col-md-9">
+      <table class="table">
+        <tr>
+          <th>Category</th>
+          <th>Title</th>
+          <th>Created By</th>
+          <th>Created Date</th>
+          <th>Contat</th>
+        </tr>
+        <?php foreach($ads as $index => $ad):?>
+        <tr>
+          <td><?= $ad[0];?></td>
+          <td><a href="ad_view.php?id=<?=$index;?>"><?= $ad[1];?></a></td>
+          <td><?= $ad[3];?></td>
+          <td><?= $ad[5];?></td>
+          <td><?= $ad[4];?></td>
+        </tr>
+        <?php endforeach;?>
+      </table>
+    </div>
 
 
   </div> <!-- /container -->
