58725ae1bcd26ca51c6a33f6b7ffbf5c3da10002
jonathan robinson <jonbrobinson@gmail.com>
2014-09-14 20:33:44 -0500

    Changed href routes

--- public/navbar.php
+++ public/navbar.php
@@ -8,13 +8,13 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#">Big Ad Site</a>
+      <a class="navbar-brand" href="ad_home.php">J's List</a>
     </div>
     <div class="navbar-collapse collapse">
       <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Home</a></li>
-        <li><a href="#about">New Post</a></li>
-        <li><a href="#contact">Contact</a></li>
+        <li class="active"><a href="#">About</a></li>
+        <li><a href="create_ad.php">New Post</a></li>
+        <li ><a href="#contact">Login</a></li>
       </ul>
     </div><!--/.nav-collapse -->
   </div>
